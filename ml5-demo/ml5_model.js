742b85b924e87f929abae59601b83a027c86ea3b
nilesh <npatra974@gmail.com>
2019-06-14 22:41:59 +0530

    added talk files

---
+++ ml5-demo/ml5_model.js
@@ -0,0 +1,24 @@
+let classifier;
+let img;
+
+function preload() {
+  classifier = ml5.imageClassifier('MobileNet');
+  img = loadImage('new_tiger.jpeg');
+}
+
+preload();
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    createDiv('Label: ' + results[0].label);
+    createDiv('Confidence: ' + nf(results[0].confidence, 0, 2));
+  }
+}
